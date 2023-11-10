86f0b553aa4ac1be08d0950d3ab3e6cc4343f2f8
Fuuzuki0307 <Yaesakura716207@gmail.com>
2023-11-10 11:07:17 +0800

    修改參數為選擇性

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('r1', function () {
     return redirect('r2');
 });
 
-Route::get('hello/{name}',function($name){
+Route::get('hello/{name?}',function($name='Everybody') {
     return 'Hello, '.$name;
 });
